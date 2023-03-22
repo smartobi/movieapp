2d1df44d81575d5c24a5f238c4b832b0f5690415
smartobi <agbawochinonso488@gmail.com>
2023-03-22 10:55:23 +0100

    modified .gitignore file

--- src/test/java/dev/farhan/movieist/MovieistApplicationTests.java
+++ src/test/java/dev/farhan/movieist/MovieistApplicationTests.java
@@ -3,7 +3,7 @@ package dev.farhan.movieist;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = MovieistApplicationTests.class)
 class MovieistApplicationTests {
 
 	@Test
